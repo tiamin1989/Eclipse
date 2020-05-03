5da1a6362ec202ab63c58e253991f2ea87b745e4
Ruslan Ramazanov <tiamin1989@yandex.ru>
2020-05-03 17:49:28 +0500

    added header

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let menu = document.querySelector('#menu');
+let list = document.querySelectorAll('#list > .header__nav-element');
+
+window.addEventListener('resize', () => {
+  for (let i = list.length - 1; i >= 0; i--) {
+    list[i].removeAttribute('style');
+  }
+});
+
+menu.addEventListener('click', () => {
+  for (let i = list.length - 1; i >= 0; i--) {
+    if (!list[i].hasAttribute('style')) {
+      list[i].style.display = 'list-item';
+      list[i].style.visibility = 'visible';
+    }
+    else list[i].removeAttribute('style');
+  }
+});
